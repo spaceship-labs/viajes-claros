138915cda63355d4b7477ed436e9f7a2258e4c4d
luis <luis19prz@gmail.com>
2014-09-30 11:26:48 -0500

    acomodo perfil viaje

--- assets/js/viajeController.js
+++ assets/js/viajeController.js
@@ -1,7 +1,7 @@
 /**
  * Created by Owner on 9/29/2014.
  */
-app.controller("viajeCTL", ['$scope', '$http','$filter',function ($scope, $http, $filter) {
+app.controller("viajeCTL", ['$scope', '$http','$filter' , function ($scope, $http, $filter) {
     $scope.viaje = window.viaje;
     $scope.funcionario = window.funcionario;
 }]);
