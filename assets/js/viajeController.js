609daee46cfaee55641b84dd158121445fb925ab
luis <luis19prz@gmail.com>
2014-10-03 18:31:17 -0500

    filtros por ciudad y por medio de transporte

--- assets/js/viajeController.js
+++ assets/js/viajeController.js
@@ -5,3 +5,15 @@ app.controller("viajeCTL", ['$scope', '$http','$filter' , function ($scope, $htt
     $scope.viaje = window.viaje;
     $scope.funcionario = window.funcionario;
 }]);
+
+app.controller("viajeSearchCTL", ['$scope', '$http','$filter' , function ($scope, $http, $filter) {
+    $scope.viajes = window.viajes;
+
+    $scope.getDateString = function(viaje){
+        var inicio = new Date(viaje.fecha_inicio_com);
+        var fin = new Date(viaje.fecha_fin_com);
+        var inicioAux = $filter('date')(inicio, 'longDate');
+        var finAux = $filter('date')(fin, 'longDate');
+        return inicioAux + " al " + finAux;
+    };
+}]);
