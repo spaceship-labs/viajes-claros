73832f5624739fc976825ba32ddf0b574fca1d3e
luis <luis19prz@gmail.com>
2014-10-03 19:48:33 -0500

    normalizando query

--- api/controllers/ViajeController.js
+++ api/controllers/ViajeController.js
@@ -26,6 +26,7 @@ module.exports = {
     },
     searchtipo : function(req,res){
         var term = req.param('filtro');
+        term = capitalizeString(term);
         if (term) {
             Viaje.find({ 'tipo_viaje' : { 'like' : "%" + term + "%" }}).exec(function(err,viajes) {
                 if (err) console.log(err);
@@ -40,7 +41,7 @@ module.exports = {
     },
     searchmedio : function(req,res){
         var term = req.param('filtro');
-        var type = req.param('tipo');
+        term = capitalizeString(term);
         if (term) {
             Viaje.find({ 'pasaje_tipo' : { 'like' : "%" + term + "%" }}).exec(function(err,viajes) {
                 if (err) console.log(err);
@@ -55,6 +56,7 @@ module.exports = {
     },
     search_hotel : function(req,res){
         var term = req.param('filtro');
+        term = capitalizeString(term);
         if (term) {
             Viaje.find({ 'hotel' : { 'like' : "%" + term + "%" }}).exec(function(err,viajes) {
                 if (err) console.log(err);
@@ -69,6 +71,7 @@ module.exports = {
     },
     search_ciudad : function(req,res){
         var term = req.param('filtro');
+        term = capitalizeString(term);
         if (term) {
             Viaje.find({ 'ciudad_destino' : { 'like' : "%" + term + "%" }}).exec(function(err,viajes) {
                 if (err) console.log(err);
@@ -82,3 +85,7 @@ module.exports = {
         }
     },
 };
+
+function capitalizeString(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
