98e07c325888ba5c860998693e28b9ca48918489
luis <luis19prz@gmail.com>
2014-10-19 14:46:44 -0500

    fixes autocomplete

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -72,6 +72,8 @@ app.controller("searhcFormCTL", ['$scope', '$http',function ($scope, $http) {
     };
      $scope.onSelectPart = function ($item, $model, $label) {
         $scope.$item = $item;
+        console.log($item);
+        window.location = '/funcionario?id=' + $item.id;
         $scope.$model = $model;
         $scope.$label = $label;
     };
